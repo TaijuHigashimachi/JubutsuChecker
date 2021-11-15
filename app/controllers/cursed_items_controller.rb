aa23e138b032a92930f85a68a3e0c8a25a1f33e6
hiroki <hrk.nishimura@gmail.com>
2021-11-15 11:11:24 +0900

    add: アップロード画像のプレビュー機能

--- app/controllers/cursed_items_controller.rb
+++ app/controllers/cursed_items_controller.rb
@@ -12,7 +12,7 @@ class CursedItemsController < ApplicationController
   def result
     if params[:upload_image]
       cookies[:jubutsuchecker] = params[:upload_image]
-      @encoded_image = Base64.encode64(params[:upload_image].read)
+      encoded_image = Base64.encode64(params[:upload_image].read)
 
       vision_api_url = URI("https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBzFRr3ebRmhyDwmg2lmMi8rAKXig94Hfo")
       headers = { "Content-Type" => "application/json" }
@@ -26,7 +26,7 @@ class CursedItemsController < ApplicationController
               }
             ],
             image: {
-              content: @encoded_image
+              content: encoded_image
             }
           }
         ]
